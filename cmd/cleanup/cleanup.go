284866b6e7639b5884f5b5e41d19a3451207b6e5
Norlihazmey Ghazali <norlihazmey.ghazali@gmail.com>
2019-06-12 13:20:31 +0800

    delete temp files

--- cmd/cleanup/cleanup.go
+++ cmd/cleanup/cleanup.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
+const (
+	tempDir = "./temp"
+)
+
+func main() {
+	files, err := ioutil.ReadDir(tempDir)
+	if err != nil {
+		log.Printf("Error reading directory: %v", err)
+	}
+
+	for _, file := range files {
+		name := fmt.Sprintf("./temp/%s", string(file.Name()))
+		if file.IsDir() {
+			if err := os.Remove(name); err != nil {
+				log.Println("Cannot remove directory: %s", file.Name())
+			}
+		} else {
+			if file.Name() != ".gitignore" {
+				if err := os.Remove(name); err != nil {
+					log.Println("Cannot remove directory: %s", file.Name())
+				}
+			}
+		}
+	}
 }
